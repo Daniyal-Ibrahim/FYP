c038c71ef60fd24a2f8d66161c9086338da44c23
Daniyal Ibrahim <daniyalgamil@gmail.com>
2021-02-05 21:16:33 +0500

    HP bar

--- TopDownRPG/Assets/Scripts/Test_HP.cs
+++ TopDownRPG/Assets/Scripts/Test_HP.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test_HP : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
     public Animator animator;
+    public Slider Slider;
     // Update is called once per frame
 
     private void Awake()
     {
         curHealth = maxHealth;
+        Slider.maxValue = maxHealth;
     }
 
     public void Damaged(int value)
     {
         curHealth -= value;
-
+        Slider.value -= value;
         if (curHealth <= 0)
         {
             Death();
@@ -33,7 +36,8 @@ public class Test_HP : MonoBehaviour
         Debug.Log("Dead");
         animator.SetTrigger("Death");
         //Destroy(this.gameObject);
-        Destroy(animator, 2);
+        Destroy(animator, 3);
+        Destroy(this.gameObject, 5);
     }
 
 
